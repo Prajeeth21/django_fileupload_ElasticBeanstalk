f1147b294682bccdb879aed5dede90926f06eb2e
Prajeeth Channa <prajeeth.channa@gmail.com>
2022-06-04 23:14:35 +0530

    HTML Form

---
+++ internproj/fileupload/forms.py
@@ -0,0 +1,6 @@
+import imp
+from django import forms
+
+class MyfileUploadForm(forms.Form):
+    file_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+
---
+++ internproj/fileupload/views.py
@@ -0,0 +1,11 @@
+import imp
+from django.shortcuts import render, HttpResponse
+
+from .forms import MyfileUploadForm
+
+# Create your views here.
+def index(request):
+    context = {
+        'form': MyfileUploadForm()
+    }
+    return render(request, 'index.html', context)
